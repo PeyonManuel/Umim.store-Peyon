a27f644b77b135bd2a02b9cc0570a1e64d5b05c6
Manuel Peyon <peyonmanuel@gmail.com>
2021-11-26 16:11:41 -0300

    create context

--- src/App.js
+++ src/App.js
@@ -2,27 +2,30 @@ import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetail
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
 import NavBar from './components/NavBar/NavBar.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CartContext } from './context/cartContext.js';
 
 function App() {
 	return (
-		<BrowserRouter>
-			<div className='App'>
-				<NavBar />
-				<main>
-					<Routes>
-						<Route
-							path='/'
-							element={<ItemListContainer greeting='Hola coder!' />}></Route>
-						<Route
-							path='/category/:categoryId'
-							element={<ItemListContainer greeting='Hola coder!' />}></Route>
-						<Route
-							path='/item/:itemId'
-							element={<ItemDetailContainer />}></Route>
-					</Routes>
-				</main>
-			</div>
-		</BrowserRouter>
+		<CartContext.Provider value={[]}>
+			<BrowserRouter>
+				<div className='App'>
+					<NavBar />
+					<main>
+						<Routes>
+							<Route
+								path='/'
+								element={<ItemListContainer greeting='Hola coder!' />}></Route>
+							<Route
+								path='/category/:categoryId'
+								element={<ItemListContainer greeting='Hola coder!' />}></Route>
+							<Route
+								path='/item/:itemId'
+								element={<ItemDetailContainer />}></Route>
+						</Routes>
+					</main>
+				</div>
+			</BrowserRouter>
+		</CartContext.Provider>
 	);
 }
 
